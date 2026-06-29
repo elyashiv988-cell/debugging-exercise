d2e81db9a1aa6327c3322b9f2bad9c8507bff47d
Elyashiv <elyashiv988@gmail.com>
2026-06-29 15:54:31 +0300

    added 1

--- find_errors.py
+++ find_errors.py
@@ -1,4 +1,5 @@
-# 
+'''
+# 1 
 age = input("Enter your age: ")
 try:
     next_year = int(age) + 1
@@ -6,3 +7,13 @@ except ValueError:
     print("Age must be a number")
     exit()
 print("Next year you will be", next_year)
+'''
+# 2
+try:
+    a = int(input("First number: "))
+    b = int(input("Second number: "))
+    print(a / b)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except ValueError:
+    print("enter a number!")
