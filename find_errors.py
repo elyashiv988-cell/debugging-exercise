991919cf75404b41c0dfb1678850f0dd3131a68b
Elyashiv <elyashiv988@gmail.com>
2026-06-29 17:53:48 +0300

    added part 2

--- find_errors.py
+++ find_errors.py
@@ -63,7 +63,7 @@ if name == "admin":
 else:
     print("Welcome user")
 # it was an error at the constuction of the code who should fixed. but try/except are for prevent unwanted but predictable situations
-    '''
+
 # 8
 price = 100
 discount = 20
@@ -83,6 +83,105 @@ if guess==password:
     print("Login successful")
 else:
     print("Wrong password")
+# 10
 
+try:
+    num1 = int(input("Number 1: "))
+    op = input("Operator: ")
+    num2 = int(input("Number 2: "))
 
+    if op == "+": 
+        print(num1 + num2)
+    elif op == "-":
+        print(num1 - num2)
+    elif op == "*":
+        print(num1 * num2)
+    elif op == "/":
+        print(num1 / num2)
+        print("cannot divede by 0")
+    else:
+        print("invalid operator")
+except ZeroDivisionError:
+    print("cannot divede by 0")
+except ValueError:
+    print("enter a number!")
+finally:
+    print("Calculator closed")
 
+# 10 extra
+celsius = input("Celsius: ")
+try:
+    fahrenheit = int(celsius) * 9 / 5 + 32
+except ValueError:
+    print("Temperature must be a number")
+print(fahrenheit)
+
+# 2
+word = input("Enter word: ")
+try:
+    print(word[0])
+except IndexError:
+    print("enter something")
+# 3
+scores = [90, 80, 100]
+total = 0
+
+for score in scores:
+    #  total = score
+    total += score
+
+average = total / len(scores)
+print(average)
+# output = 33.3
+# 90
+# logical error
+
+# 4
+products = {
+    "pen": 4,
+    "notebook": 12}
+try:
+    product = input("Product: ")
+    amount = int(input("Amount: "))
+    print(products[product] * amount)
+except ValueError:
+    print("enter a number")
+    exit()
+except KeyError:
+    print("product isn't valid")
+    exit()
+
+# 5
+files = ["data.txt", "users.csv", "notes.txt"]
+try:
+    choice = int(input("Choose file number: "))
+    print(files[choice])
+except ValueError:
+    print("enter a number")
+except IndexError:
+    print("invalid index")
+
+# 6
+numbers = [4, 10, 2, 8]
+maximum = 0
+
+for number in numbers:
+    #if number < maximum:
+    if number > maximum:
+        maximum = number
+
+print(maximum)
+# worng output = 0
+# expected =10
+# logical error
+'''
+#7
+user = {
+    "name": "Dana",
+    "age": 25}
+
+try:
+    field = input("Choose field: ")
+    print(user[field].upper())
+except KeyError:
+    print("invalid field")
